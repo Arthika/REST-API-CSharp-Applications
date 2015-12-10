d60a1e29d1ff1d34b20f28a28f8104a07ddab369
jaime@arthikatrading.com <jaime@arthikatrading.com>
2015-12-10 17:50:53 +0100

    Wrapper and 3 examples

---
+++ src/Example.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+class Example
+{
+
+    static void Main(string[] args)
+    {
+        Example0.src.Example0.Main0(args);
+        Example1.src.Example1.Main1(args);
+        Example2.src.Example2.Main2(args);
+    }
+
+}
